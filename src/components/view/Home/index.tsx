4e3a637dc01c819117c36cbc1ae6ad5494439793
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-09-11 20:40:12 +0700

    fix: UI Skeleton Home

--- src/components/view/Home/index.tsx
+++ src/components/view/Home/index.tsx
@@ -32,23 +32,40 @@ const HomeView = () => {
 
   return (
     <div className="p-2">
-      <div className="flex gap-4 items-center justify-center">
-        <Input type="text" id="search" name="search" placeholder="Search" />
-        <Link
-          href={"/cart"}
-          className="!w-fit p-2 !h-full mt-0 text-gray-600 bg-white shadow border border-grey-400"
-        >
-          <IoCartOutline />
-        </Link>
-      </div>
+      {isLoading ? (
+        <div className="flex gap-3">
+          <div className="w-11/12">
+            <Skeleton height={25} />
+          </div>
+          <div className="w-1/12">
+            <Skeleton height={25} />
+          </div>
+        </div>
+      ) : (
+        <div className="flex gap-3 items-center justify-center">
+          <Input
+            type="text"
+            id="search"
+            name="search"
+            className="!mt-0"
+            placeholder="Search"
+          />
+          <Link
+            href={"/cart"}
+            className="!w-fit p-2 !h-full mt-0 text-gray-400 bg-white shadow border border-grey-400 rounded"
+          >
+            <IoCartOutline />
+          </Link>
+        </div>
+      )}
       <div className="mt-4 flex flex-col gap-2 mb-24">
         {isLoading
           ? [...Array(5)].map((_, i) => (
-              <div key={i} className="flex gap-4 p-2 border shadow rounded">
-                <div className="w-1/5">
-                  <Skeleton width={100} height={100} />
+              <div key={i} className="flex gap-4 p-3 border shadow rounded">
+                <div className="">
+                  <Skeleton width={75} height={75} />
                 </div>
-                <div className="w-4/5">
+                <div className="w-full">
                   <Skeleton className="mb-3 " />
                   <div className="w-1/2">
                     <Skeleton />
@@ -72,7 +89,9 @@ const HomeView = () => {
                 />
                 <div className="flex flex-col">
                   <h1>{product.title}</h1>
-                  <h3>{convertIDR(product.price)}</h3>
+                  <h3 className="text-slate-600 text-sm">
+                    {convertIDR(product.price)}
+                  </h3>
                 </div>
               </div>
             ))}
