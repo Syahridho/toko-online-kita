647ecb8757c60ec1912fefdd4efe093b25eabfb9
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-09-11 13:50:42 +0700

    UI: Skeleton Home

--- src/components/view/Home/index.tsx
+++ src/components/view/Home/index.tsx
@@ -7,23 +7,27 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
+import Skeleton from "react-loading-skeleton";
 
 const HomeView = () => {
   const { push } = useRouter();
 
   const [productData, setProductData] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getProducts = async () => {
-    const { data } = await productServices.getAllProducts();
-    setProductData(data.data);
+    try {
+      const { data } = await productServices.getAllProducts();
+      setProductData(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-    try {
-      getProducts();
-    } catch (error) {
-      console.log(error);
-    }
+    getProducts();
   }, []);
 
   return (
@@ -37,26 +41,41 @@ const HomeView = () => {
           <IoCartOutline />
         </Link>
       </div>
-      <div className="mt-4 flex flex-col gap-2">
-        {productData.map((product: any) => (
-          <div
-            key={product.id}
-            className="flex gap-3 p-3 border rounded shadow-sm cursor-pointer"
-            onClick={() => push(`/products/${product.id}`)}
-          >
-            <Image
-              width={100}
-              height={100}
-              src={product.image}
-              alt={product.id}
-              className="rounded"
-            />
-            <div className="flex flex-col">
-              <h1>{product.title}</h1>
-              <h3>{convertIDR(product.price)}</h3>
-            </div>
-          </div>
-        ))}
+      <div className="mt-4 flex flex-col gap-2 mb-24">
+        {isLoading
+          ? [...Array(5)].map((_, i) => (
+              <div key={i} className="flex gap-4 p-2 border shadow rounded">
+                <div className="w-1/5">
+                  <Skeleton width={100} height={100} />
+                </div>
+                <div className="w-4/5">
+                  <Skeleton className="mb-3 " />
+                  <div className="w-1/2">
+                    <Skeleton />
+                  </div>
+                </div>
+              </div>
+            ))
+          : productData.map((product: any) => (
+              <div
+                key={product.id}
+                className="flex gap-3 p-3 border rounded shadow-sm cursor-pointer"
+                onClick={() => push(`/products/${product.id}`)}
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  src={product.image}
+                  alt={product.id}
+                  className="rounded"
+                  loading="lazy"
+                />
+                <div className="flex flex-col">
+                  <h1>{product.title}</h1>
+                  <h3>{convertIDR(product.price)}</h3>
+                </div>
+              </div>
+            ))}
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "react-loading-skeleton/dist/skeleton.css";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
